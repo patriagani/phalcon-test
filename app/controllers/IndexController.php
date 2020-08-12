6f083564bed64ef6c6f0e2730f3c0378d2a5fc73
patriagani <patria.gani@gmail.com>
2020-08-12 12:57:02 +0700

    initial commit

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+        echo "This is my first web application in Phalcon";
+    }
+
+}
+
---
+++ app/controllers/PhalconTestController.php
@@ -0,0 +1,12 @@
+<?php
+
+class PhalconTestController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+        echo "This is my first web application in Phalcon";
+    }
+
+}
+
